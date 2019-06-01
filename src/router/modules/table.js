dd1e86fed134cc1571fe72a4af0b473edb6eaecd
unknown <huangsc@qihong.io>
2019-06-01 15:07:39 +0800

    forst commit

---
+++ src/router/modules/table.js
@@ -0,0 +1,40 @@
+/** When your routing table is too long, you can split it into small modules **/
+import Layout from '@/layout'
+const tableRouter = {
+  path: '/table',
+  component: Layout,
+  //用户输入路径有输入错误的时候，我们要对这种情况进行处理，可以将所有输入错误的路径统一跳转到主页。
+  redirect: '/table/complex-table',
+  name: 'Table',
+  meta: {
+    title: 'Table',
+    icon: 'table'
+  },
+  children: [
+    {
+      path: 'dynamic-table',
+      component: () => import('@/views/table/dynamic-table/index'),
+      name: 'DynamicTable',
+      meta: { title: 'Dynamic Table' }
+    },
+    {
+      path: 'drag-table',
+      component: () => import('@/views/table/drag-table'),
+      name: 'DragTable',
+      meta: { title: 'Drag Table' }
+    },
+    {
+      path: 'inline-edit-table',
+      component: () => import('@/views/table/inline-edit-table'),
+      name: 'InlineEditTable',
+      meta: { title: 'Inline Edit' }
+    },
+    {
+      path: 'complex-table',
+      component: () => import('@/views/table/complex-table'),
+      name: 'ComplexTable',
+      meta: { title: 'Complex Table' }
+    }
+  ]
+}
+export default tableRouter
